ec007691d15010db5cfd46559ee780de5faca58a
Alex-Bosman <awb31@kent.ac.uk>
2024-12-07 17:58:27 +0000

    some slight progress on the assembler

--- assembler.py
+++ assembler.py
@@ -1,6 +1,31 @@
 import re
+
+'''
 reservedWords = ("maf", "mfa", "mif", "mfr", "mrf")
 reservedWordsFormatted = "".join([("\\b{x}\\b|".format(x=data) if index != len(reservedWords)-1 else "\\b{x}\\b".format(x=data)) for index, data in enumerate(reservedWords)])
 
 print(reservedWordsFormatted)
 print(f"^(maf) (\\br[0-9a-f]\\b|(?!(?:{reservedWordsFormatted}))\w+)$")
+'''
+
+def assemble(assemblyArray):
+
+    ## preprocessing
+
+    assemblyArray = [x.split(";")[0].strip().casefold() for x in assemblyArray if x.split(";")[0].strip().casefold() != ""]
+
+    print(assemblyArray)
+    pattern = re.compile("^(lrs)$")
+
+
+
+
+def main():
+    with open("assembly.txt", "r") as file:
+        assemblyArray = file.readlines()
+        #print(assemblyArray)
+        assemble(assemblyArray)
+
+
+if __name__ == __name__:
+    main()
