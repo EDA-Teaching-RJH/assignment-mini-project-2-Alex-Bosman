6224d9238a397d11eba389b9361bd25b2157d933
Alex-Bosman <awb31@kent.ac.uk>
2024-12-07 16:35:15 +0000

    added some more stuff and started testing regex

--- assembler.py
+++ assembler.py
@@ -1,2 +1,6 @@
 import re
+reservedWords = ("maf", "mfa", "mif", "mfr", "mrf")
+reservedWordsFormatted = "".join([("\\b{x}\\b|".format(x=data) if index != len(reservedWords)-1 else "\\b{x}\\b".format(x=data)) for index, data in enumerate(reservedWords)])
 
+print(reservedWordsFormatted)
+print(f"^(maf) (\\br[0-9a-f]\\b|(?!(?:{reservedWordsFormatted}))\w+)$")
