ef7605c7cd2293ccb476f47f1e3f92bc007e6de5
Alex-Bosman <awb31@kent.ac.uk>
2024-12-07 22:56:58 +0000

    started lexical analysis part

--- assembler.py
+++ assembler.py
@@ -8,15 +8,41 @@ print(reservedWordsFormatted)
 print(f"^(maf) (\\br[0-9a-f]\\b|(?!(?:{reservedWordsFormatted}))\w+)$")
 '''
 
+def loadInstructionDefinitions(definitionsArray):
+    pass
+
 def assemble(assemblyArray):
 
     ## preprocessing
 
+    # remove all blank lines, comments and whitespaces
     assemblyArray = [x.split(";")[0].strip().casefold() for x in assemblyArray if x.split(";")[0].strip().casefold() != ""]
 
     print(assemblyArray)
-    pattern = re.compile("^(lrs)$")
 
+    lexicalAnalysis(assemblyArray)
+    #pattern = re.compile("^(lrs)$")
+
+
+def lexicalAnalysis(assemblyArray):
+    # first create a reserved word table (syntax and any word reserved by the assembler)
+    RESERVEDWORDS = ()
+    # also create a symbols table (any user defined labels or literals)
+    SYMBOLS = []
+
+    labelRegex = re.compile("^\w+:$")
+    literalRegex = re.compile("^#define (\w+) (-\d+|\d+)$")
+    
+    # iterate through and add all labels and literals to the symbols table
+    for index, line in enumerate(assemblyArray):
+        label = labelRegex.search(line)
+        literal = literalRegex.search(line)
+        if label is not None:
+            SYMBOLS.append((index, label))
+        if literal is not None:
+            SYMBOLS.append((index, literal))
+
+    print(SYMBOLS)
 
 
 
