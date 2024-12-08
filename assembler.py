b4d16a4e97fd6210f42f94f48dbb4a1f1801a892
Alex-Bosman <awb31@kent.ac.uk>
2024-12-08 02:09:47 +0000

    added reserved word table

--- assembler.py
+++ assembler.py
@@ -20,57 +20,63 @@ class assembler:
 
         self.lexicalAnalysis(assemblyArray)
 
+        print(self.RESERVEDWORDS)
+        print(self.SYMBOLS)
+
         self.syntaxAnalysis(assemblyArray)
 
 
 
     def preProcessing(self):
-        # preprocessing
         # remove all blank lines, comments and whitespaces
         cleanedArray = [x.split(";")[0].strip().casefold() for x in self.program if x.split(";")[0].strip().casefold() != ""]
 
+        # replace pseudo-instructions with standard instructions
         # a pseudo-instruction is an instruction provided by the assembler that will get translated into multiple other real instructions
         # these instructions do not translate directly into machine code so must be translated into multiple normal instructions first
+
         # get regex definitions for all pseudo-instructions
-        #pseudoInstruction
         pseudoInstructions = [(x, re.compile(f"^{x}$"), self.ISADATA["pseudoInstructions"][x]) for x in self.ISADATA["pseudoInstructions"]]
 
-        print("")
-        print(pseudoInstructions)
-        print("")
-
+        # create an array for the output assembly
         assemblyArray = []
-        for index, line in enumerate(cleanedArray):
+        # loop through the cleanedArray
+        for line in cleanedArray:
+            # set the matchflag to false as new iteration
             MATCHFLAG = False
+            # if the current line is a pseudoinstruction, set the matchflag and also set the substitution assembly
             for x in pseudoInstructions:
                 if re.findall(x[1], line):
                     substitution = x[2]
                     MATCHFLAG = True
                     break
             if MATCHFLAG:
+                # concatinate the substitution array of instructions with the assemblyArray
                 assemblyArray += substitution
-                print("a pseudoInstruction found")
             else:
+                # else if no pseudo-instruction found then just append the regular instruction
                 assemblyArray.append(line)
-        print(assemblyArray)
-        print("")
 
         return assemblyArray
 
 
     def lexicalAnalysis(self, assemblyArray):
-        # clear symbols and reserved words tables
         # first create a reserved word table (syntax and any word reserved by the assembler and also macros)
         # also create a symbols table (any user defined labels or literals)
         self.RESERVEDWORDS = []
         self.SYMBOLS = []
-        print(self.ISADATA["instructions"])
 
+        # add all instructions, pseudoinstructions and any additional assembler reserved words to reserved word table
+        self.RESERVEDWORDS = []
+        self.RESERVEDWORDS += list(self.ISADATA["instructions"].keys()) # add instructions
+        self.RESERVEDWORDS += [x.split()[0] for x in list(self.ISADATA["pseudoInstructions"].keys())] # add pseudoinstructions
+        self.RESERVEDWORDS += [f"r{i:x}" for i in range(0, 16)] # add registers
 
         labelRegex = re.compile("^\w+:$")
         literalRegex = re.compile("^#define (\w+) (-\d+|\d+)$")
-        
+
         # iterate through and add all labels and literals to the symbols table
+        # this is done so when assembling the code, any constant reference can be replaced with the actual value and any label can be replaced with location
         for index, line in enumerate(assemblyArray):
             label = labelRegex.search(line)
             literal = literalRegex.search(line)
@@ -78,7 +84,9 @@ class assembler:
                 self.SYMBOLS.append((index, label))
             if literal is not None:
                 self.SYMBOLS.append((index, literal))
-        #print(self.SYMBOLS)
+
+        
+
 
 
 
@@ -86,15 +94,16 @@ class assembler:
         pass
 
 
+
+
 def main():
     with open("assembly.txt", "r") as file:
         program = file.readlines()
     with open("isaV4_definitions.json", "r") as file:
         instructionInfo = json.load(file)
-    
     a = assembler(instructionInfo)
     a.assemble(program)
-    #assemble(assemblyArray)
+
 
 
 if __name__ == __name__:
